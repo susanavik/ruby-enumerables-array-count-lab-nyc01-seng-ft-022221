298738fb719ad8667e0ae81b73601283d96ba8a0
Susana vik <susana.m.vik@gmail.com>
2021-02-11 15:32:39 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,10 +4,7 @@ def count_strings(array)
   end
 end
 
-# Return the total number of strings in the provided array using the count enumerable
-
 def count_empty_strings(array)
   array.count {|element| element == " "}
   end
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
